6403488d3f26d56a18a09165d8f8eac6aa93a1e4
Andreas Nilsson <andreas.nilsson@hkr.se>
2023-03-15 09:41:28 +0100

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        // Bestäm om sidorna i en triangel är:
+        //   Liksidig  - Alla sidor är lika långa
+        //   Likbent   - Två sidor är lika långa
+        //   Oliksidig - Alla sidor är olika långa
+
+        // Om vi har längden av sidorna A och B, samt vinkeln V
+        // mellan dessa kan vi räkna ut längden på sidan C med:
+        // Math.sqrt(A*A + B*B - 2*A*B*cos(V_radianer) <-- Obs! Radianer
+
+        // Vinkel i grader till radianer med:
+        // radianer = vinkel * 2 * pi / 360
+
+        // Tips: Jämför inte doubles direkt med ==
+
+
+    }
+}
